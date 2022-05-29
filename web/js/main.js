2633d3a16879c74c52ee7f1f9805ae0bbfd96724
Mizuho <mizuho128@gmail.com>
2022-05-29 16:22:04 +0900

    js/main.js remove comments

--- web/js/main.js
+++ web/js/main.js
@@ -10,17 +10,6 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
   //player = load_video('M7lc1UVf-VE');
-
-  //player = new YT.Player('player', {
-  //  /*height: '360',
-  //  width: '640',*/
-  //  videoId: 'M7lc1UVf-VE',
-  //  /*videoId: '4PEcgxNG27A',*/
-  //  events: {
-  //    'onReady': onPlayerReady,
-  //    'onStateChange': onPlayerStateChange
-  //  }
-  //});
 }
 
 function stopVideo() {
@@ -33,25 +22,8 @@ function playVideo() {
 
 
 let table = document.querySelector("#stamps");
-for (let i = 0; i < 10; i++) {
-  insert_row(table, i, 20, 200);
-}
-
-/*$(function(){
-	$.ajax({
-		url: 'https://www.google.com/search?q=jquery',    // 表示させたいコンテンツがあるページURL
-		cache: false,
-		datatype: 'html',
-		success: function(html) {
-				var h = $(html).find('#rso');    // 表示させたいコンテンツの要素を指定
-				$('#search').append(h);    // append関数で指定先の要素へ出力
-				//document.querySelector("#search")
-		}
-	});
-});*/
-
-for (var i = 0, row; row = table.rows[i]; i++) {
-  //console.log(row);
-	//row.querySelector("td.start").addEventListener('change', timechange);
-  apply_tablerow_shortcuts(row);
+let len = 10;
+for (let i = 0; i < len; i++) {
+  let s = (len-i)*10;
+  insert_row(table, i, s, s+180);
 }
