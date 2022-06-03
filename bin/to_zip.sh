a2bbfbf750851f8e21fa5072e799b20aae433d22
Mizuho <mizuho128@gmail.com>
2022-06-03 20:58:38 +0900

    to_zip.sh

---
+++ bin/to_zip.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if ! [ -d out ]; then
+  mkdir out
+fi
+
+rm out/*.zip out/*.rev
+
+git archive HEAD --output=out/recog.zip
+git rev-parse --short HEAD > out/recog.rev
+
+cd ast
+git archive HEAD --output=../out/ast.zip
+git rev-parse --short HEAD > ../out/ast.rev
