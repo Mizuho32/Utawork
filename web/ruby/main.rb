571f1ad6c006406f9c65e71ea63fb75281660ee8
Mizuho <mizuho128@gmail.com>
2022-06-11 22:00:44 +0900

    ruby/main.rb video to process function

--- web/ruby/main.rb
+++ web/ruby/main.rb
@@ -1,21 +1,37 @@
 require 'open-uri'
 require 'json'
+require 'date'
 
 #require 'sinatra/reloader' if development?
 require 'oga'
 require 'sinatra'
 require 'sinatra-websocket'
+require 'sinatra/reloader'
 
 require_relative 'utils'
 
 class App < Sinatra::Base
 
+  configure :development do
+    Utils::DATA_DIR = Pathname("test") / Utils::DATA_DIR
+    register Sinatra::Reloader
+
+    get '/restart' do
+      App.restart = true
+      App.quit!
+    end
+  end
+
   class << self
-    attr_reader :option
-    def run!(opt, **kw)
+    attr_reader :option, :list, :args
+    attr_accessor :restart
+    def run!(opt, **args)
       @option = opt
-      puts @option[:list]
-      super(**kw)
+      @args   = args
+      @list   = Utils.load_list(@option[:list])
+      #pp @list
+      @restart = false
+      super(**args)
     end
   end
 
@@ -35,19 +51,56 @@ class App < Sinatra::Base
     }
   end
 
+  get '/locked' do
+    App.list.values.select{|item| item[:lock]}.map{|item| Utils.decode_videoinfo(item)}.to_json
+  end
+
+  get '/video2process' do
+    yet_processed = App.list.values.select{|item|
+      not item[:has_segments] and not item[:lock]
+    }.first&.dup || {}
+    id = yet_processed[:video_id]&.to_sym
+    #pp App.list.values
+
+    if id then
+      if params.key?("lock") then
+        App.list[id][:lock] = DateTime.now
+        Utils.save_list(App.option[:list], App.list)
+      end
+
+      return Utils.decode_videoinfo(yet_processed).to_json
+    else
+      return "{}"
+    end
+  rescue StandardError => ex
+    status 500
+    msg = "#{ex.message}\n#{Utils.remove_bundler(ex).join("\n")}"
+    puts msg
+    {error: msg}.to_json
+  end
+
   post "/segments" do
    request.body.rewind  # 既に読まれているときのため
    data = JSON.parse request.body.read
+   id = data["video_id"].to_sym
    puts data
 
+   App.list[id][:lock] = false if App.list[id][:lock]
+
+   txt =
    if data["segments"].empty? then
      status 501
      "Empty timestamp"
    else
      Utils.save_segments(data["video_id"], data["segments"])
+     App.list[id][:has_segments] = true
      status 201
      "OK"
    end
+
+   Utils.save_list(App.option[:list], App.list)
+
+   txt
   rescue StandardError => ex
     status 500
     "#{ex.message}\n#{Utils.remove_bundler(ex).join("\n")}"
