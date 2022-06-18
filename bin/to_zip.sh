12170a69b165dc529adf9755e018fc998363e102
Mizuho <mizuho128@gmail.com>
2022-06-18 21:16:09 +0900

    bin/to_zip.sh show git commit id

--- bin/to_zip.sh
+++ bin/to_zip.sh
@@ -9,6 +9,10 @@ rm out/*.zip out/*.rev
 git archive HEAD --output=out/recog.zip
 git rev-parse --short HEAD > out/recog.rev
 
+cat out/recog.rev
+git rev-parse --short HEAD
+
 cd ast
 git archive HEAD --output=../out/ast.zip
 git rev-parse --short HEAD > ../out/ast.rev
+
