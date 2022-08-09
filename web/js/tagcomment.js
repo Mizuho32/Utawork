87c4d325015c8f9ae01ef857160f52c5d5ef32f2
Mizuho <mizuho128@gmail.com>
2022-08-09 17:47:54 +0900

    js/tagcomment.js copy to clipboard to dirty method

--- web/js/tagcomment.js
+++ web/js/tagcomment.js
@@ -3,7 +3,8 @@ function copypaste() {
   let copyText = document.querySelector("code");
 
   /* Copy the text inside the text field */
-  navigator.clipboard.writeText(copyText.innerText);
+  //navigator.clipboard.writeText(copyText.innerText);
+  copyToClipboard(copyText.innerText);
 
   /* Alert the copied text */
   //alert("Copied the text: " + copyText.innerText);
@@ -13,4 +14,13 @@ function copypaste() {
   let yt = `https://youtube.com/watch?v=${video_id}`;
   window.open(yt, '_blank').focus();
   //window.location.assign(yt); // for mobile?
+}
+
+function copyToClipboard(text) {
+  let elem = document.createElement('textarea');
+  elem.value = text;
+  document.body.appendChild(elem);
+  elem.select();
+  document.execCommand('copy');
+  document.body.removeChild(elem);
 }
