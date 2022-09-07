0fae65541279b597564c76463c764374891d42b2
Mizuho <mizuho128@gmail.com>
2022-09-07 21:20:12 +0900

    server.rb list with comment cmd option for tagcomment

--- web/server.rb
+++ web/server.rb
@@ -7,6 +7,7 @@ parser = OptionParser.new
 parser.on('-l list.yaml', "--list", "List of youtube videos") {|v| option[:list] = v }
 parser.on('-d', "--debug", "Debug mode") { option[:debug] = true }
 parser.on('-s google sheet id and gid', "--sheet-link", "id and gid of Google sheet for commenting") {|v| option[:sheet_link] = v }
+parser.on('--lc list.yaml', "--list-with-comments", "List with video comments") {|v| option[:list_with_comments] = v }
 
 # For sinatra help
 if not ARGV.map{|el| el =~ /h(elp)?/}.any? then
