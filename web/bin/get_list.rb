ecf5704f935a3e1b382fc656a1110c113f04f03e
Mizuho <mizuho128@gmail.com>
2022-09-17 16:29:34 +0900

    bin/get_list.rb handle Deleted list, open yaml bug, :name -> :title bug fix

--- web/bin/get_list.rb
+++ web/bin/get_list.rb
@@ -21,6 +21,7 @@ def get_until(tube, id, title_reg, video_id_reg=/^$/, max_results: 5)
   page_token = nil
   videos = []
 
+
   loop do
     res = tube.list_playlist_items('snippet,contentDetails', playlist_id: id.to_s, max_results: max_results, page_token: page_token)
     page_token = res.next_page_token
@@ -32,7 +33,7 @@ def get_until(tube, id, title_reg, video_id_reg=/^$/, max_results: 5)
     }.take_while{|(title, video_id)|
       not (title_reg =~ title or video_id_reg =~ video_id)
     }.each{|(title, video_id)|
-      next if title == "Private video"
+      next if title == "Private video" || title == "Deleted video"
       puts title
       detail = get_video_details(tube, video_id).first
       videos << {title: title, video_id: video_id, published_at: detail.snippet.published_at.new_offset(Time.now.getlocal.zone), duration: detail.content_details.duration}
@@ -51,19 +52,20 @@ end
 # ARGV: list.yaml  apikey  listid
 
 listyaml = Pathname(ARGV.first)
-list = YAML.load_file(ARGV.first) rescue []
+list = YAML.load_file(ARGV.first, permitted_classes: [OpenStruct, Symbol, DateTime, Time]) rescue []
 
 tube = Google::Apis::YoutubeV3::YouTubeService.new
 tube.key = ARGV[1]
 
-last_name = list.empty? ? /^$/ :  Regexp.new(Regexp.escape(list[-1][:name]))
+last_name = list.empty? ? /^$/ :  Regexp.new(Regexp.escape(list.first[:title]))
+
 videos = get_until(tube, ARGV[2].to_s, last_name, max_results: 10)
   .sort{|l,r| l[:published_at] <=> r[:published_at]}
   .reverse
 
 
-puts "Delta: \n#{videos.map{|itm| itm[:name]}.join("\n")}\n------"
+puts "Delta: \n#{videos.map{|itm| itm[:title]}.join("\n")}\n------"
 
-yml = (list + videos).to_yaml
+yml = (videos + list).to_yaml
 #puts yml
 File.write(listyaml, yml)
