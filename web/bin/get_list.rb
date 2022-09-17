da0055e1f84fd4b8aa8fdd2d0dae76399c56f1b8
Mizuho <mizuho128@gmail.com>
2022-09-17 16:34:10 +0900

    bin/get_list.rb use yt_utils.rb

--- web/bin/get_list.rb
+++ web/bin/get_list.rb
@@ -1,52 +1,9 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-require 'yaml'
-require 'pathname'
 require 'date'
 
-require 'google/apis/youtube_v3'
-
-def get_video_details(youtube, ids, max_results = 10, part: "id,snippet,contentDetails")
-  ids = [ids] if ids.is_a? String
-
-  all_items = ids.each_slice(max_results).inject([]){|items, ids_sub|
-    items += youtube.list_videos(part, id: ids_sub.join(",")).items
-    items
-  }
-  return all_items
-end
-
-def get_until(tube, id, title_reg, video_id_reg=/^$/, max_results: 5)
-  page_token = nil
-  videos = []
-
-
-  loop do
-    res = tube.list_playlist_items('snippet,contentDetails', playlist_id: id.to_s, max_results: max_results, page_token: page_token)
-    page_token = res.next_page_token
-
-    filtered = res.items.map{|itm|
-      title, video_id = itm.snippet.title, itm.content_details.video_id
-
-      [title, video_id]
-    }.take_while{|(title, video_id)|
-      not (title_reg =~ title or video_id_reg =~ video_id)
-    }.each{|(title, video_id)|
-      next if title == "Private video" || title == "Deleted video"
-      puts title
-      detail = get_video_details(tube, video_id).first
-      videos << {title: title, video_id: video_id, published_at: detail.snippet.published_at.new_offset(Time.now.getlocal.zone), duration: detail.content_details.duration}
-    }
-
-    is_hit = filtered.size < max_results
-
-    break if is_hit or page_token.to_s.empty?
-  end
-
-  return videos
-end
-
+require_relative '../ruby/yt_utils'
 
 
 # ARGV: list.yaml  apikey  listid
@@ -59,7 +16,7 @@ tube.key = ARGV[1]
 
 last_name = list.empty? ? /^$/ :  Regexp.new(Regexp.escape(list.first[:title]))
 
-videos = get_until(tube, ARGV[2].to_s, last_name, max_results: 10)
+videos = YTUtils.get_until(tube, ARGV[2].to_s, last_name, max_results: 10)
   .sort{|l,r| l[:published_at] <=> r[:published_at]}
   .reverse
 
